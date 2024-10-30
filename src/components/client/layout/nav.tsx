08a97b494468709aa0431f66ba7fd8de6ce259c6
larry <xspanni@gmail.com>
2024-10-30 14:06:05 +0800

    调整导航条，增加语言切换功能

--- src/components/client/layout/nav.tsx
+++ src/components/client/layout/nav.tsx
@@ -2,13 +2,17 @@ import './nav.scss'
 
 export function NavComponent({path, lang}: { path: string, lang: string }) {
     const activeClass = (item: string) => {
-        if (!item) {
-            return path === `/${lang}` ? 'active' : ''
-        }
-        return path.startsWith(`/${lang}${item}`) ? 'active' : ''
+        return lang === item ? 'active' : ''
     }
     return (
         <nav className={'navComponent'}>
+            <div className={'brand'}>
+                <img src={'/images/logo.jpg'} alt={'logo'}/>
+            </div>
+            <div className={'languages'}>
+                <a href={`/en`} className={activeClass('en')}>English</a>
+                <a href={`/zh`} className={activeClass('zh')}>中文</a>
+            </div>
         </nav>
     )
 }
--- src/middleware.ts
+++ src/middleware.ts
@@ -54,5 +54,5 @@ export function middleware(request: NextRequest) {
 }
 
 export const config = {
-    matcher: ['/((?!api|_next/static|_next/image|assets|favicon.ico|sw.js|site.webmanifest).*)'],
+    matcher: ['/((?!api|_next/static|_next/image|images|assets|favicon.ico|sw.js|site.webmanifest).*)'],
 }
